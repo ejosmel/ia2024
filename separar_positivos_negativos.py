3c2853fddfa92f4416dadc78360cfa2d3ae184bc
ejosmel <ejosmel@gmail.com>
2024-11-08 19:44:49 +0100

    Delete separar_positivos_negativos.py

--- separar_positivos_negativos.py
+++
@@ -1,5 +0,0 @@
-def separar_positivos_negativos(numeros): 
-		numeros.sort()
-negativos = [num for num in numeros if num < 0] 
-positivos = [num for num in numeros if num >= 0] 
-return negativos, positivos 
